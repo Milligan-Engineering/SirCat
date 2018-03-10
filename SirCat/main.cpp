5f668f7cdb017e8c4218d46c91d24028dd4d5faf
cjwilliamsMilligan <cjwilliams@my.milligan.edu>
2018-03-09 19:29:57 -0500

    Add the rest of the core logic, placeholder comments for function calls of functions that have yet to be implemented. Add a few new functions and refine existing functions.

--- SirCat/main.cpp
+++ SirCat/main.cpp
@@ -15,79 +15,141 @@
 
 using namespace std;
 
-void pauseBeforeExit();
-//Postcondition: Console will wait for a user-inputted character before exiting.
-
-bool getSteamDir(char steamDir[]);
-//Precondition: 
-//Postcondition: 
+bool getSteamDir(char steamDir[_MAX_PATH]);
+//Precondition: The array steamDir is modifiable.
+//Postcondition: If a valid registry value for a Steam installation is found, steamDir is filled with the installation directory.
+//               Returns true if a valid registry value is found, and false otherwise.
 
 int concatCharArrays(char cArray1[], char cArray2[], char concatArray[], int sizeConcatArray);
-//Precondition: 
-//Postcondition: 
+//Precondition: The arrays cArray1 and cArray2 are null-terminated.
+//              The sum of filled elements of cArray1 and cArray2 is less than or equal to the size of array concatArray.
+//              concatArray is modifiable.
+//              sizeConcatArray is the size of concatArray.
+//Postcondition: concatArray is filled with the concatenation of cArray1 and cArray2, and terminated with a null character.
+//               cArray1 and cArray2 can be the same array as concatArray to append or prepend to the original input array.
+//               Returns the index of the last filled element in concatArray.
 
 int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][_MAX_PATH], int maxSearchResults,
-                  const char ignoreChars[], int numIgnoreChars);
-//Precondition: 
-//Postcondition: 
+                  const char ignoreChars[] = "", int numIgnoreChars = 0);
+//Precondition: searchTerm should not contain any whitespace characters.
+//              The file input stream searchFile has been successfully connected to a file with ifstream::open member function.
+//              The two-dimensional array searchResult is modifiable.
+//              maxSearchResults is less than or equal to the first dimension in searchResult.
+//              If the array ignoreChars is passed, numIgnoreChars is less than or equal to the size of ignoreChars.
+//Postcondition: searchFile is searched by looping through non-whitespace strings that are separated by whitespace characters.
+//               When a string matches searchTerm, a *searchResults[_MAX_PATH] array is filled with the line's remaining characters.
+//               Characters passed in ignoreChars are not copied to *searchResults[_MAX_PATH] and escaped backslashes are unescaped.
+//               searchResult is filled, up to maxSearchResults number of *searchResults[_MAX_PATH] arrays.
+//               Returns the number of *searchResults[_MAX_PATH] arrays that are at least partially filled.
 
-bool checkCsgoInstall(char testDir[]);
-//Precondition: 
-//Postcondition: 
+bool checkCsgoInstall(char testDir[_MAX_PATH]);
+//Precondition: The array testDir is modifiable.
+//Postcondition: If a CS:GO manifest file is found in testDir, it is parsed for the CS:GO installation directory.
+//               If the installation directory is parsed from the manifest file, testDir is filled with it.
+//               Returns true if the installation directory is parsed from the manifest file, and false otherwise.
 
-bool searchSteamLibraries(char testDir[]);
-//Precondition: 
-//Postcondition: 
+bool searchSteamLibraries(char testDir[_MAX_PATH]);
+//Precondition: The array testDir is modifiable.
+//Postcondition: If Steam library folders file is found in testDir, it is parsed to enumerate up to 9 alternate library paths to search.
+//               testDir is filled with one alternate library path at a time and passed to the checkCsgoInstall function.
+//               Returns true and stops enumerating if checkCsgoInstall returns true for an alternate library path, and false otherwise.
 
 void readArchiveFile();
-//Postcondition: 
+//Precondition: The archive file exists and can be successfully connected to a file input stream with ifstream::open member function.
+//Postcondition: Parses archived hitbox and weapon data from the archive file and stores it in program memory.
+
+int takeOnlyOneChar();
+//Precondition: The returned value is intended to be a decimal digit.
+//Postcondition: Returns int equal to first input character if it is a decimal digit, and 0 otherwise, discarding the rest of the input.
 
 int main()
 {
-	char steamDir[_MAX_PATH];
+	int menuOption = 0;
 	char testDir[_MAX_PATH];
-	bool bRevertToArchiveFile = true;
+	bool bExit = false;
 
-	if (getSteamDir(steamDir))
+	do
 	{
-		char defaultFolder[] = "\\steamapps";
+		bool bRevertToArchiveFile = true;
 
-		cout << "Steam installation found in directory: \n" << steamDir << endl << endl;
+		if (menuOption < 2)
+		{
+			if (getSteamDir(testDir))
+			{
+				char steamappsFolder[] = "\\steamapps";
 
-		concatCharArrays(steamDir, defaultFolder, testDir, _MAX_PATH);
-		if (checkCsgoInstall(testDir)) //CSGO found in default Steam library
-			bRevertToArchiveFile = false;
-		else if (searchSteamLibraries(testDir)) //CSGO found in alternate Steam library
-			bRevertToArchiveFile = false;
-	}
+				cout << "Steam installation found in directory:\n" << testDir << endl << endl;
 
-	if (bRevertToArchiveFile)
-	{
-		cout << "CS:GO installation not found. Reading hitbox and weapon data from archive file.\n\n";
+				concatCharArrays(testDir, steamappsFolder, testDir, _MAX_PATH);
+				if (checkCsgoInstall(testDir) //CSGO found in default Steam library
+					|| searchSteamLibraries(testDir)) //CSGO found in alternate Steam library
+				{
+					cout << "CS:GO installation found in directory:\n" << testDir << endl << endl;
 
-		readArchiveFile();
-	}
-	else
-	{
-		cout << "CS:GO installation found in directory: \n" << testDir << endl << endl;
+					//readWeaponFile
+					//unpackModels
+					//decompileModels
+					//readModelFiles
+					//overwriteArchiveFile
+					bRevertToArchiveFile = false;
+				}
+			}
 
-		//readWeaponFile();
-	}
+			if (bRevertToArchiveFile)
+			{
+				cout << "CS:GO installation not found. Reading hitbox and weapon data from archive file.\n\n";
 
-	pauseBeforeExit();
+				readArchiveFile();
+			}
+
+			//hitboxList
+			//weaponList
+		}
+
+		if (menuOption < 3)
+		{
+			//userModifyData
+		}
+
+		//enter distance
+
+		//calcIdealFreq
+
+		bool bValidMenuOption;
+
+		do
+		{
+			bValidMenuOption = false;
+
+			cout << endl << endl;
+			cout << "1 - start over for a fresh calculation, starting from actual game data\n";
+			cout << "2 - modify hitbox and weapon data for another calculation\n";
+			cout << "3 - pick distance for another calculation with the same hitbox and weapon data\n";
+			cout << "4 - exit the program\n\n";
+			cout << "Please enter a choice from the preceding menu options: ";
+
+			switch (menuOption = takeOnlyOneChar())
+			{
+			case 1:
+			case 2:
+			case 3:
+				cout << endl << endl << endl << endl << endl;
+				bValidMenuOption = true;
+				break;
+			case 4:
+				bExit = true;
+				bValidMenuOption = true;
+				break;
+			default:
+				cout << endl << "That is not a valid menu option.\n\n";
+			}
+		} while (!bValidMenuOption);
+	} while (!bExit);
 
 	return 0;
 }
 
-void pauseBeforeExit()
-{
-	char exitLetter;
-
-	cout << "\n\nType a letter to exit: ";
-	cin >> exitLetter;
-}
-
-bool getSteamDir(char steamDir[])
+bool getSteamDir(char steamDir[_MAX_PATH])
 {
 	bool bFoundRegValue = false;
 
@@ -97,9 +159,9 @@ bool getSteamDir(char steamDir[])
 	if (bFoundRegValue == true)
 	{
 		char steamDirTemp[] = "C:\\Program Files (x86)\\Steam"; //Replace with RegQueryValueEx output
+		char blank[] = "";
 
-		for (int i = 0; i < 29; ++i) //Size of steamDirTemp is 29
-			steamDir[i] = steamDirTemp[i];
+		concatCharArrays(steamDirTemp, blank, steamDir, _MAX_PATH);
 	}
 
 	return bFoundRegValue;
@@ -109,40 +171,36 @@ int concatCharArrays(char cArray1[], char cArray2[], char concatArray[], int siz
 {
 	int concatIndex = 0;
 
-	for (int i = 0; cArray1[i] != '\0' && concatIndex < sizeConcatArray; ++i)
+	for (int i = 0; cArray1[i] != '\0' && concatIndex < sizeConcatArray - 1; ++i)
 	{
 		concatArray[concatIndex] = cArray1[i];
 		++concatIndex;
 	}
 
-	for (int i = 0; cArray2[i] != '\0' && concatIndex < sizeConcatArray; ++i)
+	for (int i = 0; cArray2[i] != '\0' && concatIndex < sizeConcatArray - 1; ++i)
 	{
 		concatArray[concatIndex] = cArray2[i];
 		++concatIndex;
 	}
 
-	if (concatIndex < sizeConcatArray)
-	{
-		concatArray[concatIndex] = '\0';
-		++concatIndex;
-	}
+	concatArray[concatIndex] = '\0';
 
 	return concatIndex;
 }
 
 int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][_MAX_PATH], int maxSearchResults,
-                  const char ignoreChars[] = "", int numIgnoreChars = 0)
+                  const char ignoreChars[], int numIgnoreChars)
 {
 	int instancesFound = 0;
 	string testString;
 
 	searchFile >> testString;
-	while (!searchFile.eof()
-		&& instancesFound < maxSearchResults)
+	while (!searchFile.eof() && instancesFound < maxSearchResults)
 	{
 		if (testString == searchTerm)
 		{
 			char character;
+			char characterLast = '\0';
 			int i = 0;
 
 			searchFile.get(character);
@@ -152,7 +210,8 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 
 				for (int j = 0; j < numIgnoreChars; ++j)
 				{
-					if (character == ignoreChars[j])
+					if (character == ignoreChars[j]
+						|| (characterLast == '\\' && character == '\\')) //Detect escaped backslashes
 						bIgnoreChar = true;
 				}
 
@@ -162,6 +221,7 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 					++i;
 				}
 
+				characterLast = character;
 				searchFile.get(character);
 			}
 			searchResults[instancesFound][i] = '\0';
@@ -175,7 +235,7 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 	return instancesFound;
 }
 
-bool checkCsgoInstall(char testDir[])
+bool checkCsgoInstall(char testDir[_MAX_PATH])
 {
 	char manifestFileName[] = "\\appmanifest_730.acf";
 	char manifestFilePath[_MAX_PATH];
@@ -187,13 +247,13 @@ bool checkCsgoInstall(char testDir[])
 	if (!manifestFile.fail())
 	{
 		char searchResult[1][_MAX_PATH];
-		const char ignoreChars[] = { '\t', '\"' }; //Ignore tabs and quotations for searchResult
+		const char ignoreChars[] = { '\t', '\"', '\0' }; //Ignore tabs and quotations for searchResult
 
-		if (static_cast<bool>(parseTextFile("\"installdir\"", manifestFile, searchResult, 1, ignoreChars, 2)))
+		if (static_cast<bool>(parseTextFile(static_cast<string>("\"installdir\""), manifestFile, searchResult, 1, ignoreChars, 2)))
 		{
 			bFoundCsgoInstall = true;
 			char installSubDir[] = "\\common\\";
-			
+
 			concatCharArrays(testDir, installSubDir, testDir, _MAX_PATH);
 			concatCharArrays(testDir, searchResult[0], testDir, _MAX_PATH);
 		}
@@ -204,14 +264,40 @@ bool checkCsgoInstall(char testDir[])
 	return bFoundCsgoInstall;
 }
 
-bool searchSteamLibraries(char testDir[])
+bool searchSteamLibraries(char testDir[_MAX_PATH])
 {
+	char libFoldersFileName[] = "\\libraryfolders.vdf";
+	char libFoldersFilePath[_MAX_PATH];
+	ifstream libFoldersFile;
 	bool bFoundCsgoInstall = false;
 
-	//Enumerate alternate Steam libraries from defaultDir\libraryfolders.vdf
+	concatCharArrays(testDir, libFoldersFileName, libFoldersFilePath, _MAX_PATH);
+	libFoldersFile.open(libFoldersFilePath);
+	if (!libFoldersFile.fail())
+	{
+		for (int i = 1; i < 10; i++)
+		{
+			char searchTerm[] = { '\"', static_cast<char>(i + static_cast<int>('0')), '\"', '\0' };
+			char searchResult[1][_MAX_PATH];
+			const char ignoreChars[] = { '\t', '\"', '\0' }; //Ignore tabs and quotations for searchResult
 
-	//Loop calls to checkCsgoInstall for each alternate Steam library
-	//If CS:GO installation is found, set bFoundCsgoInstall to true and exit loop
+			if (static_cast<bool>(parseTextFile(static_cast<string>(searchTerm), libFoldersFile, searchResult, 1, ignoreChars, 2)))
+			{
+				char steamappsFolder[] = "\\steamapps";
+
+				concatCharArrays(searchResult[0], steamappsFolder, testDir, _MAX_PATH);
+				if (checkCsgoInstall(testDir))
+				{
+					bFoundCsgoInstall = true;
+					break;
+				}
+			}
+			else
+				break;
+		}
+
+		libFoldersFile.close();
+	}
 
 	return bFoundCsgoInstall;
 }
@@ -223,8 +309,11 @@ void readArchiveFile()
 	archiveFile.open("archive.csv");
 	if (archiveFile.fail())
 	{
-		cout << endl << "Archive file failed to open.\n\n";
-		pauseBeforeExit();
+		char exitLetter;
+
+		cout << endl << "Archive file failed to open.\n\n\n";
+		cout << endl << "Type a letter to exit: ";
+		cin >> exitLetter;
 		exit(1);
 	}
 
@@ -234,9 +323,34 @@ void readArchiveFile()
 	archiveFile.get(character);
 	while (!archiveFile.eof())
 	{
-		//Parse archive file contents
+		//Parse archive file contents and store to program memory
+		//Waiting to decide on file format and handling of memory
 
 		archiveFile.get(character);
 	}
 	archiveFile.close();
+}
+
+int takeOnlyOneChar()
+{
+	int menuOption = 0;
+	char menuOptionChar = '\0';
+
+	for (bool bFirstChar = true; menuOptionChar != '\n';)
+	{
+		cin.get(menuOptionChar);
+
+		if (bFirstChar)
+		{
+			menuOption = static_cast<int>(menuOptionChar) - static_cast<int>('0');
+			if (menuOption < 0 || menuOption > 9)
+				menuOption = 0;
+
+			bFirstChar = false;
+		}
+		else if (menuOptionChar != '\n')
+			menuOption = 0;
+	}
+
+	return menuOption;
 }
