3096838b1e13ad292ef3b26ff51ce33b191f0b8a
cjwilliamsMilligan <cjwilliams@my.milligan.edu>
2018-03-13 22:54:14 -0400

    Add functions to extract and count numbers of rows and columns for delimited text data files. Rearranged functions to put ones that could be reused in other code near the top and ones specific to this program near the bottom. bReadWeaponFile still needs work.

--- SirCat/main.cpp
+++ SirCat/main.cpp
@@ -20,6 +20,8 @@ const int k_num_weapons = 26;
 const int k_weapon_name_length = 21;
 const int k_num_attributes = 21;
 const int k_attribute_length = 30;
+const int k_num_unparsed_attributes = 70;
+const int k_data_length = 10;
 const char k_sir[] = "archiveSirData.csv";
 const char k_bbox[] = "archiveBboxData.csv";
 
@@ -27,10 +29,45 @@ bool bUserMenu(int &menuOption);
 //Postcondition: menuOption from the calling function is updated according to the user's input.
 	//Returns true if the program should continue or false if the program should exit.
 
-bool bGetSteamDir(char steamDir[_MAX_PATH]);
-//Precondition: The array steamDir is modifiable.
-//Postcondition: If a valid registry value for a Steam installation is found, steamDir is filled with the installation directory.
-	//Returns true if a valid registry value is found, and false otherwise.
+int takeOnlyOneChar();
+//Precondition: The returned value is intended to be a decimal digit.
+//Postcondition: Returns int equal to input char decimal digit, if immediately followed by input of new-line, and 0 otherwise.
+
+int charDecimalDigitToInt(const char charDecimalDigit);
+//Precondition: 
+//Postcondition: 
+
+char intDecimalDigitToChar(const int intDecimalDeigit);
+//Precondition: 
+//Postcondition:
+
+void typeLetterToExit();
+//Precondition: 
+//Postcondition:
+
+bool bParseDelimitedSlice(ifstream &delimitedFile, const char k_file_name[], const bool bSliceIsRow,
+	string parsedSlice[], const int k_max_elements,
+	const int k_skip_to_element = 1, const char k_delimiter = ',', const int k_num_slice = 1);
+//Precondition: 
+//Postcondition:
+
+int getNumDelimitedColumns(ifstream &delimitedFile, const char k_file_name[],
+	const char k_delimiter = ',', const int k_num_row = 1);
+//Precondition: 
+//Postcondition:
+
+int getNumDelimitedRows(ifstream &delimitedFile, const char k_file_name[],
+	const char k_delimiter = ',', const int k_num_column = 1);
+//Precondition: 
+//Postcondition:
+
+void skipToRowNum(ifstream &delimitedFile, char &character, const int k_num_row);
+//Precondition: 
+//Postcondition:
+
+bool bSkipToColumnNum(ifstream &delimitedFile, char &character, const char k_delimiter, const int k_num_column);
+//Precondition: 
+//Postcondition:
 
 int concatCharArrays(char cArray1[], char cArray2[], char concatArray[], const int k_size_concat_array);
 //Precondition: The strings cArray1 and cArray2 are null-terminated.
@@ -55,6 +92,11 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 	//If k_terminal_search_char is passed, the function will return if and when that character is found.
 	//Returns the number of null-terminated strings *searchResults[_MAX_PATH] filled in the array of strings searchResults.
 
+bool bGetSteamDir(char steamDir[_MAX_PATH]);
+//Precondition: The array steamDir is modifiable.
+//Postcondition: If a valid registry value for a Steam installation is found, steamDir is filled with the installation directory.
+//Returns true if a valid registry value is found, and false otherwise.
+
 bool bCheckCsgoInstall(char testDir[_MAX_PATH]);
 //Precondition: The array testDir is modifiable.
 //Postcondition: If a CS:GO manifest file is found in testDir, it is parsed for the CS:GO installation directory.
@@ -67,34 +109,36 @@ bool bSearchSteamLibs(char testDir[_MAX_PATH]);
 	//testDir is filled with one alternate library path at a time and passed to the bCheckCsgoInstall function.
 	//Returns true and stops enumerating if bCheckCsgoInstall returns true for an alternate library path, and false otherwise.
 
-void getWeaponNamesOrAttributes(bool bPickWeaponNames, string whichArray[]);
+bool bReadWeaponFile(char csgoDir[_MAX_PATH], string weaponNames[k_num_weapons], string weaponAttributes[k_num_attributes]);
 //Precondition: 
-//Postcondition: 
-
-bool bReadWeaponFile(char csgoDir[_MAX_PATH]);
-//Precondition: 
-//Postcondition: 
-
-void openArchiveFile(ifstream &archiveFile, const char k_which_archive[]);
-//Precondition: 
-//Postcondition: 
+//Postcondition:
 
 void readArchiveFiles();
 //Precondition: The archive file can be successfully connected to a file input stream with ifstream::open member function.
 //Postcondition: Parses archived hitbox and weapon data from the archive file and stores it in program memory.
 
-int takeOnlyOneChar();
-//Precondition: The returned value is intended to be a decimal digit.
-//Postcondition: Returns int equal to input char decimal digit, if immediately followed by input of new-line, and 0 otherwise.
+void openArchiveFile(ifstream &archiveFile, const char k_which_archive[]);
+//Precondition: 
+//Postcondition: 
 
 int main()
 {
 	int menuOption = 0;
+	ifstream archiveSir;
 	char testDir[_MAX_PATH];
+	string weaponNames[k_num_weapons];
+	string weaponAttributes[k_num_attributes];
 
 	do
 	{
 		bool bRevertToArchiveFile = true;
+
+		if (!bParseDelimitedSlice(archiveSir, k_sir, false, weaponNames, k_num_weapons, 2)
+			|| !bParseDelimitedSlice(archiveSir, k_sir, true, weaponAttributes, k_num_attributes, 2))
+		{
+			cout << "Failed to retrieve weapon or attribute list.\n\n\n";
+			typeLetterToExit();
+		}
 
 		if (menuOption < 2)
 		{
@@ -109,8 +153,8 @@ int main()
 					|| bSearchSteamLibs(testDir)) //CSGO found in alternate Steam library
 				{
 					cout << "CS:GO installation found in directory:\n" << testDir << endl << endl;
-					
-					bReadWeaponFile(testDir);
+
+					bReadWeaponFile(testDir, weaponNames, weaponAttributes);
 
 					//unpackModels
 
@@ -190,22 +234,212 @@ bool bUserMenu(int &menuOption)
 	return bContinue;
 }
 
-bool bGetSteamDir(char steamDir[_MAX_PATH])
+int takeOnlyOneChar()
 {
-	bool bFoundRegValue = false;
+	const int k_invalid = 0;
 
-	//Use Windows functions RegOpenKeyEx, RegQueryValueEx, and RegCloseKey to search for the Steam directory if it exists
-	bFoundRegValue = true; //Set to true until Windows functions are added
+	int menuOption = k_invalid;
+	char menuOptionChar = '\0';
+	bool bFirstChar = true;
 
-	if (bFoundRegValue == true)
+	while (menuOptionChar != '\n')
+		//for (bool bFirstChar = true; menuOptionChar != '\n'; ) //Third expression intentionally left blank
 	{
-		char steamDirTemp[] = "C:\\Program Files (x86)\\Steam"; //Replace with RegQueryValueEx output
-		char blank[] = "";
+		cin.get(menuOptionChar);
 
-		concatCharArrays(steamDirTemp, blank, steamDir, _MAX_PATH);
+		if (bFirstChar)
+		{
+			menuOption = charDecimalDigitToInt(menuOptionChar);
+			if (menuOption < 0 || menuOption > 9)
+				menuOption = k_invalid;
+
+			bFirstChar = false;
+		}
+		else if (menuOptionChar != '\n') //More than one char input
+			menuOption = k_invalid;
 	}
 
-	return bFoundRegValue;
+	return menuOption;
+}
+
+int charDecimalDigitToInt(const char charDecimalDigit)
+{
+	return (static_cast<int>(charDecimalDigit) - static_cast<int>('0'));
+}
+
+char intDecimalDigitToChar(const int intDecimalDeigit)
+{
+	return (static_cast<char>(intDecimalDeigit + static_cast<int>('0')));
+}
+
+void typeLetterToExit()
+{
+	char exitLetter;
+
+	cout << endl << "Type a letter to exit: ";
+	cin >> exitLetter;
+	exit(1);
+}
+
+bool bParseDelimitedSlice(ifstream &delimitedFile, const char k_file_name[], const bool bSliceIsRow,
+	string parsedSlice[], const int k_max_elements,
+	const int k_skip_to_element, const char k_delimiter, const int k_num_slice)
+{
+	const int k_max_element_length = k_attribute_length; //Use k_attribute_length since it is longer than k_weapon_name_length
+
+	bool bParseSuccess = false;
+	char character;
+	int maxNumSlice;
+	int numElements;
+
+	if (bSliceIsRow)
+	{
+		maxNumSlice = getNumDelimitedRows(delimitedFile, k_file_name, k_delimiter, k_skip_to_element);
+		numElements = getNumDelimitedColumns(delimitedFile, k_file_name, k_delimiter, k_num_slice) - k_skip_to_element + 1;
+	}
+	else
+	{
+		maxNumSlice = getNumDelimitedColumns(delimitedFile, k_file_name, k_delimiter, k_skip_to_element);
+		numElements = getNumDelimitedRows(delimitedFile, k_file_name, k_delimiter, k_num_slice) - k_skip_to_element + 1;
+	}
+
+	if (k_num_slice <= maxNumSlice) //Check that the slice exists
+	{
+		delimitedFile.open(k_file_name);
+		if (!delimitedFile.fail())
+		{
+			if (bSliceIsRow) //Skip to first element to parse when parsing a row
+			{
+				skipToRowNum(delimitedFile, character, k_num_slice);
+				bSkipToColumnNum(delimitedFile, character, k_delimiter, k_skip_to_element);
+			}
+			else //Skip to the desire row only when parsing a column
+				skipToRowNum(delimitedFile, character, k_skip_to_element);
+
+			for (int i = 0; i < numElements && i < k_max_elements; i++) //Begin parsing slice into parsedSlice
+			{
+				int j;
+				char parsedSliceElement[k_max_element_length];
+
+				if (!bSliceIsRow) //Skip to desired column only when parsing a column
+					bSkipToColumnNum(delimitedFile, character, k_delimiter, k_num_slice);
+
+				for (j = 0; j < k_max_element_length && !delimitedFile.eof(); ++j)
+				{
+					delimitedFile.get(character);
+
+					if (character == k_delimiter || character == '\n')
+						break;
+					else
+						parsedSliceElement[j] = character;
+				}
+
+				parsedSliceElement[j] = '\0'; //Add terminal null character to character strings
+				parsedSlice[i] = static_cast<string>(parsedSliceElement);
+
+				while (!bSliceIsRow && !delimitedFile.eof() && character != '\n') //Skip to next row for parsing a column
+					delimitedFile.get(character);
+			}
+
+			bParseSuccess = true;
+		}
+		delimitedFile.close();
+	}
+
+	return bParseSuccess;
+}
+
+int getNumDelimitedColumns(ifstream &delimitedFile, const char k_file_name[],
+	const char k_delimiter, const int k_num_row)
+{
+	int numColumns = 0;
+	char character;
+
+	delimitedFile.open(k_file_name);
+	if (!delimitedFile.fail())
+	{
+		skipToRowNum(delimitedFile, character, k_num_row);
+
+		if (!delimitedFile.eof())
+		{
+			++numColumns;
+			delimitedFile.get(character);
+
+			while (!delimitedFile.eof() && character != '\n')
+			{
+				if (character == k_delimiter)
+					++numColumns;
+
+				delimitedFile.get(character);
+			}
+		}
+	}
+	delimitedFile.close();
+
+	return numColumns;
+}
+
+int getNumDelimitedRows(ifstream &delimitedFile, const char k_file_name[],
+	const char k_delimiter, const int k_num_column)
+{
+	int numRows = 0;
+	char character;
+	bool bTooFewColumns = false; //Becomes true when the number of rows has been enumerated
+
+	delimitedFile.open(k_file_name);
+	if (!delimitedFile.fail())
+	{
+		while (!delimitedFile.eof())
+		{
+			if (bTooFewColumns = bSkipToColumnNum(delimitedFile, character, k_delimiter, k_num_column)) //Single = is intentional
+				break;
+
+			if (!delimitedFile.eof())
+			{
+				++numRows;
+
+				do //Skip to next row
+				{
+					delimitedFile.get(character);
+				} while (!delimitedFile.eof() && character != '\n');
+			}
+		}
+	}
+	delimitedFile.close();
+
+	return numRows;
+}
+
+void skipToRowNum(ifstream &delimitedFile, char &character, const int k_num_row)
+{
+	for (int i = 1; i < k_num_row; ++i)
+	{
+		do
+		{
+			delimitedFile.get(character);
+		} while (!delimitedFile.eof() && character != '\n');
+	}
+}
+
+bool bSkipToColumnNum(ifstream &delimitedFile, char &character, const char k_delimiter, const int k_num_column)
+{
+	bool bTooFewColumns = false;
+
+	for (int i = 1; i < k_num_column; ++i)
+	{
+		do
+		{
+			delimitedFile.get(character);
+
+			if (character == '\n')
+			{
+				bTooFewColumns = true;
+				break;
+			}
+		} while (!delimitedFile.eof() && character != k_delimiter);
+	}
+
+	return bTooFewColumns;
 }
 
 int concatCharArrays(char cArray1[], char cArray2[], char concatArray[], const int k_size_concat_array)
@@ -234,10 +468,10 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 {
 	int instancesFound = 0;
 	string testString;
-	char character = '\n';
+	char character;
 
 	searchFile >> testString;
-	while (!searchFile.eof() && instancesFound < k_max_search_results && character != k_terminal_search_char)
+	while (!searchFile.eof() && instancesFound < k_max_search_results)
 	{
 		if (testString == searchTerm)
 		{
@@ -245,8 +479,8 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 			int i = 0;
 
 			searchFile.get(character);
-			while (!searchFile.eof() && character != '\n') //Fill search result entry until new-line or end of file
-			{
+			while (!searchFile.eof() && character != '\n' && character != k_terminal_search_char)
+			{ //Fill search result entry until new-line or end of file
 				bool bIgnoreChar = false;
 
 				for (int j = 0; j < k_num_ignore_chars; ++j)
@@ -271,12 +505,33 @@ int parseTextFile(string searchTerm, ifstream &searchFile, char searchResults[][
 			searchResults[instancesFound][i] = '\0'; //Terminate string with null character
 
 			++instancesFound;
+
+			if (character == k_terminal_search_char)
+				break;
 		}
 		else
 			searchFile >> testString;
 	}
 
 	return instancesFound;
+}
+
+bool bGetSteamDir(char steamDir[_MAX_PATH])
+{
+	bool bFoundRegValue = false;
+
+	//Use Windows functions RegOpenKeyEx, RegQueryValueEx, and RegCloseKey to search for the Steam directory if it exists
+	bFoundRegValue = true; //Set to true until Windows functions are added
+
+	if (bFoundRegValue == true)
+	{
+		char steamDirTemp[] = "C:\\Program Files (x86)\\Steam"; //Replace with RegQueryValueEx output
+		char blank[] = "";
+
+		concatCharArrays(steamDirTemp, blank, steamDir, _MAX_PATH);
+	}
+
+	return bFoundRegValue;
 }
 
 bool bCheckCsgoInstall(char testDir[_MAX_PATH])
@@ -316,7 +571,7 @@ bool bSearchSteamLibs(char testDir[_MAX_PATH])
 	{
 		for (int i = 1; i < 10; i++)
 		{
-			char searchTerm[] = { '\"', static_cast<char>(i + static_cast<int>('0')), '\"', '\0' };
+			char searchTerm[] = { '\"', intDecimalDigitToChar(i), '\"', '\0' };
 			char searchResult[1][_MAX_PATH];
 
 			if (static_cast<bool>(parseTextFile(static_cast<string>(searchTerm), libFile, searchResult, 1, "\t\"\0", 2)))
@@ -340,94 +595,94 @@ bool bSearchSteamLibs(char testDir[_MAX_PATH])
 	return bFoundCsgoInstall;
 }
 
-void getWeaponNamesOrAttributes(bool bPickWeaponNames, string whichArray[])
+bool bReadWeaponFile(char csgoDir[_MAX_PATH], string weaponNames[k_num_weapons], string weaponAttributes[k_num_attributes])
 {
-	const int k_nums[] = { k_num_attributes, k_num_weapons };
-	const int k_lengths[] = { k_attribute_length, k_weapon_name_length };
-	const int k_pick_weapon_names = static_cast<const int>(bPickWeaponNames);
-	const char k_skip_characters[] = { '\n', ',' };
-
-	ifstream sirFile;
-	char weaponName[k_weapon_name_length];
-	char weaponAttribute[k_attribute_length];
-
-	openArchiveFile(sirFile, k_sir);
-	for (int i = 0; i < k_nums[k_pick_weapon_names]; ++i)
-	{
-		char character;
-		int j = 0;
-
-		if (i == 0 || !bPickWeaponNames) //Only during the first iteration for weapon names
-		{
-			do //Skip the first entry or to the next line in the CSV file
-			{
-				sirFile.get(character);
-			} while (character != k_skip_characters[k_pick_weapon_names]);
-		}
-
-		sirFile.get(character);
-		while (character != ',' && character != '\n' && j < k_lengths[k_pick_weapon_names])
-		{ //Read the next CSV entry
-			if (bPickWeaponNames)
-				weaponName[j] = character;
-			else
-				weaponAttribute[j] = character;
-
-			sirFile.get(character);
-			++j;
-		}
-
-		if (bPickWeaponNames) //Add terminal null character to character string and set element in string array
-		{
-			weaponName[j] = '\0';
-			whichArray[i] = static_cast<string>(weaponName);
-		}
-		else
-		{
-			weaponAttribute[j] = '\0';
-			whichArray[i] = static_cast<string>(weaponAttribute);
-		}
-	}
-	sirFile.close();
-}
-
-bool bReadWeaponFile(char csgoDir[_MAX_PATH])
-{
-	bool bParsedWeaponFile = false;
-	string weaponNames[k_num_weapons];
-	string weaponAttributes[k_num_attributes];
+	bool bParseSuccess = false;
 	ifstream weaponFile;
-
-	getWeaponNamesOrAttributes(true, weaponNames);
-	getWeaponNamesOrAttributes(false, weaponAttributes);
 
 	weaponFile.open(static_cast<string>(csgoDir) + static_cast<string>("\\csgo\\scripts\\items\\items_game.txt"));
 	if (!weaponFile.fail())
 	{
 		char searchResult[1][_MAX_PATH];
 
-		if (static_cast<bool>(parseTextFile(static_cast<string>("CSGO_Type_Machinegun"), weaponFile, searchResult, 1)))
-		{ //Start collecting weapon data to parse
-			
+		if (static_cast<bool>(parseTextFile(static_cast<string>("\"#CSGO_Type_Machinegun\""), weaponFile, searchResult, 1)))
+		{
+			char unparsedWeaponData[k_num_weapons][k_num_unparsed_attributes][_MAX_PATH];
+			char parsedWeaponData[k_num_weapons][k_num_unparsed_attributes][k_data_length];
+			int numUnparsedReturns[k_num_weapons];
+			char numbers[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+			for (int i = 0; i < k_num_weapons; ++i) //Collect weapon data for each weapon
+			{
+				string searchTerm = "\"" + weaponNames[i] + "_prefab\"";
+
+				parseTextFile(searchTerm, weaponFile, searchResult, 1); //Skip to weapon name
+				numUnparsedReturns[i] = parseTextFile(static_cast<string>("\"attributes\""), weaponFile, unparsedWeaponData[i], _MAX_PATH, "\t\"\0", 2, '}');
+			}
+
+			for (int g = 0; g < k_num_weapons; ++g)
+			{
+				for (int i = 0; i < numUnparsedReturns[g]; ++i)
+				{
+					bool bNumberInReturn = false;
+
+					for (int j = 0; j < k_attribute_length; ++j)
+					{
+						for (int k = 0; k < 10; ++k)
+						{
+							if (unparsedWeaponData[g][i][j] == numbers[k])
+							{
+								int l;
+
+								bNumberInReturn = true;
+
+								for (l = 0; l < k_data_length && unparsedWeaponData[g][i][j + l] != '\0'; ++l)
+								{
+									parsedWeaponData[g][i][l] = unparsedWeaponData[g][i][j + l];
+									unparsedWeaponData[g][i][j + l] = '\0';
+								}
+								parsedWeaponData[g][i][l] = '\0';
+
+								while (unparsedWeaponData[g][i][j + l] != '\0')
+									unparsedWeaponData[g][i][j + l] = '\0';
+
+								break;
+							}
+						}
+
+						if (bNumberInReturn)
+							break;
+					}
+				}
+
+				cout << weaponNames[g] << " - \n";
+
+				for (int h = 0; h < k_num_attributes; ++h)
+				{
+					bool bAttributeNotFound = true;
+
+					for (int i = 0; i < numUnparsedReturns[g]; ++i)
+					{
+						if (unparsedWeaponData[g][i] == weaponAttributes[h])
+						{
+							bAttributeNotFound = false;
+							cout << weaponAttributes[h] << ": " << parsedWeaponData[g][i] << endl;
+							break;
+						}
+					}
+
+					if (bAttributeNotFound)
+						cout << weaponAttributes[h] << ": " << endl;
+				}
+
+				cout << endl;
+			}
 		}
+
 		weaponFile.close();
 	}
 
-	return bParsedWeaponFile;
-}
-
-void openArchiveFile(ifstream &archiveFile, const char k_which_archive[])
-{
-	archiveFile.open(k_which_archive);
-	if (archiveFile.fail())
-	{
-		char exitLetter;
-
-		cout << endl << "Archive file " << k_which_archive << " failed to open.\n\n\n";
-		cout << endl << "Type a letter to exit: ";
-		cin >> exitLetter;
-		exit(1);
-	}
+	return bParseSuccess;
 }
 
 void readArchiveFiles()
@@ -450,31 +705,12 @@ void readArchiveFiles()
 	sirFile.close();
 }
 
-int takeOnlyOneChar()
+void openArchiveFile(ifstream &archiveFile, const char k_which_archive[])
 {
-	const int k_invalid = 0;
-
-	int menuOption = k_invalid;
-	char menuOptionChar = '\0';
-	bool bFirstChar = true;
-
-	while(menuOptionChar != '\n')
-	//for (bool bFirstChar = true; menuOptionChar != '\n'; ) //Third expression intentionally left blank
+	archiveFile.open(k_which_archive);
+	if (archiveFile.fail())
 	{
-		cin.get(menuOptionChar);
-
-		if (bFirstChar)
-		{
-			//Preserve decimal digit while converting char to int
-			menuOption = static_cast<int>(menuOptionChar) - static_cast<int>('0');
-			if (menuOption < 0 || menuOption > 9)
-				menuOption = k_invalid;
-
-			bFirstChar = false;
-		}
-		else if (menuOptionChar != '\n') //More than one char input
-			menuOption = k_invalid;
+		cout << endl << "Archive file " << k_which_archive << " failed to open.\n\n\n";
+		typeLetterToExit();
 	}
-
-	return menuOption;
 }
