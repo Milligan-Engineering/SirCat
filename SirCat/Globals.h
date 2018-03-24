ba46eda5c1c2236398d935130429d5edb382f3a1
cjwilliamsMilligan <cjwilliams@my.milligan.edu>
2018-03-23 23:09:22 -0400

    Revert use of precompiled header.

---
+++ SirCat/Globals.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Globals
+{
+public:
+	int get_k_max_path();
+	int get_k_num_weap();
+	int get_k_num_attr();
+	string get_k_sir();
+	string get_k_bbox();
+};
